08742b4b68230def642b73b5f635d76d4628646a
Brian Joseph Petro <brian@smartconnections.app>
2025-06-28 19:22:24 -0400

    Refactor main.js by removing commented-out code and unused environment configuration

--- main.js
+++ main.js
@@ -2,22 +2,6 @@ import { Plugin, Notice, addIcon } from "obsidian";
 import { SmartEnv } from "obsidian-smart-env";
 import { merge_env_config } from "obsidian-smart-env";
 import { smart_env_config as smart_context_env_config } from "smart-context-obsidian/smart_env.config.js";
-// // chat model
-// import { SmartChatModel } from "smart-chat-model";
-// import {
-//   SmartChatModelAnthropicAdapter,
-//   SmartChatModelAzureAdapter,
-//   // SmartChatModelCohereAdapter,
-//   SmartChatModelCustomAdapter,
-//   SmartChatModelGeminiAdapter,
-//   SmartChatModelGroqAdapter,
-//   SmartChatModelLmStudioAdapter,
-//   SmartChatModelOllamaAdapter,
-//   SmartChatModelOpenaiAdapter,
-//   SmartChatModelOpenRouterAdapter,
-// } from "smart-chat-model/adapters.js";
-// import { SmartHttpRequest, SmartHttpObsidianRequestAdapter } from "smart-http-request";
-// import { requestUrl } from "obsidian";
 
 import { smart_env_config } from "./smart_env.config.js";
 
@@ -26,42 +10,9 @@ import { SmartChatSettingTab } from "./src/settings_tab.js";
 
 export default class SmartChatPlugin extends Plugin {
   compiled_smart_env_config = smart_env_config;
-  /**
-   * Example environment config. Adjust to your needs.
-   * @type {Object}
-   */
-  smart_env_config = {
-    collections: {
-      // smart_sources: {
-      //   process_embed_queue: true,
-      // }
-    },
-    modules: {
-      // smart_chat_model: {
-      //   class: SmartChatModel,
-      //   // DEPRECATED FORMAT: will be changed (requires SmartModel adapters getters update)
-      //   adapters: {
-      //     anthropic: SmartChatModelAnthropicAdapter,
-      //     azure: SmartChatModelAzureAdapter,
-      //     custom: SmartChatModelCustomAdapter,
-      //     gemini: SmartChatModelGeminiAdapter,
-      //     groq: SmartChatModelGroqAdapter,
-      //     lm_studio: SmartChatModelLmStudioAdapter,
-      //     ollama: SmartChatModelOllamaAdapter,
-      //     open_router: SmartChatModelOpenRouterAdapter,
-      //     openai: SmartChatModelOpenaiAdapter,
-      //   },
-      //   http_adapter: new SmartHttpRequest({
-      //     adapter: SmartHttpObsidianRequestAdapter,
-      //     obsidian_request_url: requestUrl,
-      //   }),
-      // },
-    },
-  };
 
   onload() {
-    const merged_config = merge_env_config(this.compiled_smart_env_config, this.smart_env_config);
-    merge_env_config(merged_config, smart_context_env_config);
+    const merged_config = merge_env_config(this.compiled_smart_env_config, smart_context_env_config);
     SmartEnv.create(this, merged_config);
     this.app.workspace.onLayoutReady(this.initialize.bind(this));
   }
