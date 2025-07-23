25c5fd0a6e169cc04c4ec852ecf1d982fa1af96e
Brian Joseph Petro <brian@smartconnections.app>
2025-07-22 22:28:44 -0400

    Update package-lock.json to specify file paths for dependencies and adjust versions; enhance smart_actions.js to export SmartActions.

--- src/collections/smart_actions.js
+++ src/collections/smart_actions.js
@@ -1,6 +1,7 @@
-import { smart_actions } from 'smart-actions';
+import { smart_actions, SmartActions } from 'smart-actions';
 import * as lookup_context from '../actions/lookup_context.js';
 smart_actions.default_actions = {
   lookup_context,
 };
+export { SmartActions };
 export default smart_actions;
