9c100b1c2474aa63ca9d005e13bbc6c13eddbe3d
Brian Joseph Petro <brian@smartconnections.app>
2025-11-08 12:57:29 -0500

    Fix import path for SmartCompletionContextAdapter in smart_completions.js

--- src/collections/smart_completions.js
+++ src/collections/smart_completions.js
@@ -1,6 +1,6 @@
 import { smart_completions } from "smart-completions";
 import { ThreadCompletionAdapter } from "../adapters/smart-completions/thread.js";
-import { SmartCompletionContextAdapter } from "smart-completions/adapters/context.js";
+import { SmartCompletionContextAdapter } from "../adapters/smart-completions/context.js";
 smart_completions.completion_adapters['ThreadCompletionAdapter'] = ThreadCompletionAdapter;
 smart_completions.completion_adapters['SmartCompletionContextAdapter'] = SmartCompletionContextAdapter;
 
